897f730029f71d117e4989b1b58704c1d0def44f
ahmedreda6 <ahmed.reda55767@gmail.com>
2024-06-20 09:17:21 +0300

    0

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,5 @@
-import 'package:brain_box/Features/splash/presentaion/views/splash_view.dart';
 import 'package:brain_box/core/utils/app_router.dart';
 import 'package:brain_box/firebase_options.dart';
-import 'package:brain_box/core/helper/navigationbar.dart';
-import 'package:brain_box/screens/Profile.dart';
-import 'package:brain_box/screens/add_member.dart';
-import 'package:brain_box/screens/alert_datails.dart';
-import 'package:brain_box/screens/editinformation.dart';
-import 'package:brain_box/screens/forgot_password.dart';
-import 'package:brain_box/screens/home.dart';
-import 'package:brain_box/screens/login.dart';
-import 'package:brain_box/screens/preferences.dart';
-import 'package:brain_box/screens/sensor.dart';
-import 'package:brain_box/screens/sign-up.dart';
-import 'package:brain_box/screens/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -36,20 +23,3 @@ class DripAlart extends StatelessWidget {
     );
   }
 }
-  // routes: {
-  //       'loginScreen': (context) => const Loginview(),
-  //       'signUpScreen': (context) => const SignUpView(),
-  //       'WelcomeScreen': (context) => const Welcomeview(),
-  //       'HomeScreen': (context) => const HomeView(),
-  //       'SensorScreen': (context) => const SensorView(),
-  //       'ProfileScreen': (context) => const ProfileView(),
-  //       'BottomNavBar': (context) => const BottomNavBar(),
-  //       'PreferencesScreen': (context) => const PreferencesView(),
-  //       'EditInformationScreen': (context) => const EditInformationView(),
-  //       'ForgotPasswordScreen': (context) => const ForgotPasswordView(),
-  //       'AlertDatailsScreen': (context) => const AlertDatailsView(),
-  //       AddMemberView.id: (context) => const AddMemberView(),
-  //               'Splash': (context) => const SplashView(),
-
-  //     },
-  //     initialRoute: 'Splash',
